ab4ac0427318456d008c348e740f027ea86b9530
mohankrishna-challapalli <mohankrishna.challapalli@gmail.com>
2023-02-25 23:39:23 +0530

    updated

---
+++ src/Basics/Loops.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class Loops {
+    public static void main(String[] args) {
+        int j=1;
+        while(j==0){
+            System.out.println("Three times");
+            j++;
+        }
+        for(int i=0;i<4;i++){
+            System.out.println(i);
+        }
+        do{
+            System.out.println("Three time");
+        }while(j==0);
+
+    }
+}
